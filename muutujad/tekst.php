66b4689bad2f199cf00b333fb3c2352578146b3f
Anna Karutina <anna.karutina@khk.ee>
2019-09-30 15:43:50 +0300

    testiväärtused

---
+++ muutujad/tekst.php
@@ -0,0 +1,21 @@
+<?php
+// muutujate loomine
+$enimi = 'Karin';
+$pnimi = "Eegreid";
+$vanus = 37;
+$kaal = 69.5;
+
+//kahekordsed jutumärgid
+echo "$enimi on $vanus aastane<br>";
+
+//ühekordsed jutumärgid
+echo $enimi.' on '.$vanus.' aastane<br>';
+
+//muutuja käänamine
+echo "${enimi}il on kiire auto!<br>";
+
+//jutumärkide kuvamine
+echo '"Welcome to Estonia"<br>';
+echo "Shakespeare'i teosed<br>";
+
+echo "\"It's My Life\" - Dr. Alban<br>";
