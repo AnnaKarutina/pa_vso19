7da2225ce6451140b82ae4862050d910aebd232e
Anna Karutina <anna.karutina@khk.ee>
2019-10-04 09:07:04 +0300

    Ülesanne 1 - võimalik lahendus

---
+++ muutujad/ulesanne1.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * file name: ulesanne1.php;
+ * autor: anna.karutina;
+ * date: 04.10.2019;
+ */
+// muutujate defineerimine ja väärtuste omistamine
+$nimi = 'Anna';
+$vanus = 38;
+$sugu = 'naine';
+// väljastamine koos tekstiga
+echo 'Minu nimi on '.$nimi.'<br>';
+echo 'Olen '.$vanus.' aastat vana<br>';
+echo 'Olen '.$sugu.'<br>';
+// kujundi väljastamine
+echo '<hr>';
+echo '&nbsp;&nbsp;\_____/<br>';
+echo '&nbsp;&nbsp;(&nbsp;@,@&nbsp;)<br>';
+echo '&nbsp;&nbsp;((&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;))<br>';
+echo '&nbsp;&nbsp;__,,_,,__<br>';
